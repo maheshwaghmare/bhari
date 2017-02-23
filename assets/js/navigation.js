51e563adc872f98b2d7e9a7cfeeabef42316df3d
maheshwaghmare <mwaghmare7@gmail.com>
2017-02-23 23:08:41 +0530

    Tweak: Spacing & Comments.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,6 +4,7 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
+
 ( function() {
 	var container, button, menu, links, subMenus, i, len;
 
