8aefbca08c32cce6239149f5e09fd69d30679da0
maheshwaghmare <mwaghmare7@gmail.com>
2017-02-25 17:55:36 +0530

    Tweak: Updated post tags location.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -123,7 +123,7 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 					'after'  => '',
 				),
 				'tag' => array(
-					'before' => ( BHARI_SUPPORT_FONTAWESOME ) ? '<i class="fa fa-tags"></i> ' : '<span class="label">' . __( 'Tags ', 'bhari' ) . '</span>',
+					'before' => '<span class="label">' . __( 'Tagged in ', 'bhari' ) . '</span>',
 					'after'  => '',
 				),
 			),
@@ -143,13 +143,11 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 						'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 					);
 					$meta_author  = $meta_args['meta']['author']['before'];
-					$meta_author .= '<span class="byline">';
-					$meta_author .= $byline;
-					$meta_author .= '</span>';
+					$meta_author .= '<span class="byline">' . $byline . '</span>';
 					$meta_author .= $meta_args['meta']['author']['after'];
 
 					// Set author meta.
-					$meta_data['author'] = $meta_author;
+					$meta_data['author'] = '<span class="meta-author">' .$meta_author. '</span>';
 				break;
 
 				/**
@@ -174,14 +172,11 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 					);
 
 					$meta_date  = $meta_args['meta']['date']['before'];
-					$meta_date .= '<span class="posted-on">';
-					$meta_date .= $posted_on; // WPCS: XSS OK.
-					$meta_date .= '</span>';
+					$meta_date .= '<span class="posted-on">' . $posted_on . '</span>';
 					$meta_date .= $meta_args['meta']['date']['after'];
 
 					// Set date meta.
-					$meta_data['date'] = $meta_date;
-
+					$meta_data['date'] = '<span class="meta-date">' .$meta_date. '</span>';
 				break;
 
 				/**
@@ -200,7 +195,7 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 						$meta_category .= $meta_args['meta']['category']['after'];
 
 						// Set category meta.
-						$meta_data['category'] = $meta_category;
+						$meta_data['category'] = '<span class="meta-category">' .$meta_category. '</span>';
 					}
 
 				break;
@@ -212,7 +207,7 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 				 */
 				case 'tags':
 
-					$tags_list = get_the_tag_list( '', esc_html__( ', ', 'bhari' ) );
+					$tags_list = get_the_tag_list();
 
 					if ( $tags_list ) {
 						$meta_tags = $meta_args['meta']['tag']['before'];
@@ -220,7 +215,7 @@ if ( ! function_exists( 'bhari_post_meta' ) ) :
 						$meta_tags .= $meta_args['meta']['tag']['after'];
 
 						// Set tag meta.
-						$meta_data['tag'] = $meta_tags;
+						$meta_data['tag'] = '<span class="meta-tag">' .$meta_tags. '</span>';
 					}
 
 				break;
@@ -272,27 +267,27 @@ if ( ! function_exists( 'bhari_entry_footer_contents' ) ) :
 	function bhari_entry_footer_contents() {
 
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+
 			$edit_icon = ( BHARI_SUPPORT_FONTAWESOME ) ? '<i class="fa fa-comments"></i> ' : '';
 			echo '<span class="comments-link"> ';
 			echo $edit_icon;
+
 			/* translators: %s: post title */
 			comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'bhari' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
 			echo '</span>';
+
+			// Separator.
+			echo '<span class="sep">/</span>';
 		}
 
 		/**
-		 * Edit link
+		 * Print post meta
+		 *
+		 * @see bhari_post_meta($meta_list, $before, $after)
 		 */
-		$edit_icon = ( BHARI_SUPPORT_FONTAWESOME ) ? '<i class="fa fa-edit"></i> ' : '';
-		edit_post_link(
-			sprintf(
-				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'bhari' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">' . $edit_icon,
-			'</span>'
-		);
+		bhari_post_meta( array( 'tags' ) );
+
+
 	}
 	add_action( 'bhari_entry_footer', 'bhari_entry_footer_contents' );
 
